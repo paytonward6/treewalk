1286eef95a41554fd0e62314136101d7f8a9430a
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2022-12-24 10:32:20 -0600

    added some functions for human readable size

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,6 @@ mod treewalk {
         //uniq: bool,
     }
 
-
     //TODO: implement uniq for sizequery
     pub fn largest_file(children: Vec<PathBuf>) -> SizeQuery {
         let mut result = SizeQuery{name: None, size: 0};
@@ -56,28 +55,109 @@ mod treewalk {
         }
         result
     }
-}
 
+    pub fn smallest_file(children: Vec<PathBuf>) -> SizeQuery {
+        let mut result = SizeQuery{name: None, size: u64::MAX};
+        for child in children {
+            if child.is_file() {
+                let size = child.as_path().metadata();
+                if let Ok(size) = size {
+                    if size.len() < result.size {
+                        result.name = Some(child);
+                        result.size = size.len();
+                    }
+                }
+            }
+        }
+        result
+    }
+
+    pub fn smallest_dir(children: Vec<PathBuf>) -> SizeQuery {
+        let mut result = SizeQuery{name: None, size: u64::MAX};
+        for child in children {
+            if child.is_dir() {
+                let size = child.as_path().metadata();
+                if let Ok(size) = size {
+                    if size.len() < result.size {
+                        result.name = Some(child);
+                        result.size = size.len();
+                    }
+                }
+            }
+        }
+        result
+    }
+
+    pub fn recursively_list_contents(path: &PathBuf, children: &mut Vec<PathBuf>) -> () {
+        if path.is_dir() {
+            for child in path.read_dir().expect("Attempt to read contents of directory has failed!") {
+                if let Ok(child) = child {
+                    let child_path = child.path();
+                    children.push(child_path.to_path_buf());
+                    recursively_list_contents(&child_path, children);
+                }
+            }
+        }
+    }
+
+    pub fn digit_len(num: u64) -> u64 {
+        let mut first = num;
+        let mut i = 0;
+        if num == 0 {
+            return 0
+        }
+        while first != 0 {
+            first /= 10;
+            i += 1;
+        }
+        i
+    }
+
+    pub fn human_readable(num: u64) -> String {
+        let result = String::from("");
+        let num_str = String::from(&num.to_string());
+        match digit_len(num) {
+            1..=4 => result + &num_str + "B",
+            5..=7 =>  result + &num_str[..(num_str.len() - 3)] + "KB",
+            8..=12 =>  result + &num_str[..(num_str.len() - 6)] + "MB",
+            13..=16 => result + &num_str[..(num_str.len() - 9)] + "MB",
+            _ => 0.0.to_string(),
+        }
+    }
+
+}
 
 fn main() {
     use std::env;
-    use std::path::Path;
+    use std::path::{Path, PathBuf};
     use treewalk::*;
 
     let args: Vec<String> = env::args().collect();
     if args.len() >= 2 {
-        let path = Path::new(&args[1]);
-        let f = largest_file(children(path));
+        //let path = Path::new(&args[1]);
 
-        if let Some(file_name) = &f.name {
-            println!("{:?}: {:?}", file_name, f.size);
-        }
+        //let mut children: Vec<PathBuf> = Vec::new();
+        //recursively_list_contents(&path.to_path_buf(), &mut children);
 
-        let d = largest_dir(children(path));
-        if let Some(file_name) = &d.name {
-            println!("{:?}: {:?}", file_name, d.size);
-        }
-    } else {
+        //let small = largest_file(children);
+        //if let Some(file_name) = &small.name {
+        //    println!("{:?}: {:?}", file_name, small.size);
+        //}
+
+        let num: u64 = 10_000_000;
+        println!("{}", human_readable(num));
+
+        assert_eq!(human_readable(1_000), "1000B");
+        assert_eq!(human_readable(999), "999B");
+        assert_eq!(human_readable(10_000), "10KB");
+        assert_eq!(human_readable(100_000), "100KB");
+        assert_eq!(human_readable(500_000), "500KB");
+        assert_eq!(human_readable(1_000_000), "1000KB");
+        assert_eq!(human_readable(10_000_000), "10MB");
+        assert_eq!(human_readable(1_000_000_000), "1000MB");
+    }
+    else {
         println!("No files found.");
     }
+
 }
